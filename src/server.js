5e32195757da1e205fa56133484a4c8b6b29b532
Anastasiia <ambarnova94@gmail.com>
2025-10-09 20:49:52 +0200

    fixed homework

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -17,11 +18,8 @@ app.use(logger);
 
 app.use(notesRoutes);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
